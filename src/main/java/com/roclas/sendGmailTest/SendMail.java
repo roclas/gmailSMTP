e1db6492f7cac850470cb1f5dce2a78f18cd615a
carlos <carlos@es-l-chernandez>
2012-12-27 12:50:36 +0100

    initial commit

---
+++ src/main/java/com/roclas/sendGmailTest/SendMail.java
@@ -0,0 +1,84 @@
+package com.roclas.sendGmailTest;
+import java.util.Properties;
+ 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+public class SendMail{
+	final static String user="test@gmail.com";
+	final static String password="mypassword";
+	
+	public static void sendMailSSL(String[] args) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
+		Session session = Session.getDefaultInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(user,password);
+				}
+			});
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from@no-spam.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("myemail@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler," +
+					"\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+ 
+	public static void sendMailTSL(String[] args) {
+ 
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("myemail@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
